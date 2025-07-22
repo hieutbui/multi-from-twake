04ac3f417371aba0c941f9cb847f1f961e9956ca
Bùi Trung Hiếu <trunghieu.bui201@gmail.com>
2025-07-22 12:30:55 +0700

    MFT-37: Add recent chats section to new private chat view and improve contact list UI

--- lib/pages/new_chat_or_group/new_chat_or_group.dart
+++ lib/pages/new_chat_or_group/new_chat_or_group.dart
@@ -5,10 +5,13 @@ import 'package:fluffychat/presentation/mixins/go_to_group_chat_mixin.dart';
 import 'package:fluffychat/presentation/mixins/invite_external_contact_mixin.dart';
 import 'package:fluffychat/presentation/model/contact/presentation_contact.dart';
 import 'package:fluffychat/presentation/model/search/presentation_search.dart';
+import 'package:fluffychat/utils/matrix_sdk_extensions/matrix_locals.dart';
 import 'package:fluffychat/widgets/matrix.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:go_router/go_router.dart';
 import 'package:matrix/matrix.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 class NewChatOrGroup extends StatefulWidget {
   const NewChatOrGroup({super.key});
@@ -23,13 +26,25 @@ class NewChatOrGroupController extends State<NewChatOrGroup>
         GoToGroupChatMixin,
         GoToDraftChatMixin,
         InviteExternalContactMixin {
+  Client get client => Matrix.of(context).client;
+
   @override
   void initState() {
     super.initState();
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      if (mounted) {
+        initialFetchContacts(
+          context: context,
+          client: client,
+          matrixLocalizations: MatrixLocals(L10n.of(context)!),
+        );
+      }
+    });
   }
 
   @override
   void dispose() {
+    disposeContactsMixin();
     super.dispose();
   }
 
